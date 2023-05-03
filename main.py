be5109e8028a65b5beca1dadf5d66a5ec80ec485
Andy Yang <71013200+Y-jiji@users.noreply.github.com>
2023-05-03 22:23:32 +0800

    Fix: replace SGD with Adam

--- main.py
+++ main.py
@@ -8,19 +8,20 @@ device = 'cuda:0'
 
 if __name__ == '__main__':
     model = WPLRLeNet5(10).to(device).to(torch.double)
-    optim = SGD(model.parameters(), lr=1e-17)
+    optim = Adam(model.parameters())
     trans = Compose([ToTensor(), (lambda x: x.to(device).to(torch.double))])
     rolling_acc = 0.1
-    rolling_dam = 0.99999
+    rolling_dam = 0.999
     for epoch in range(1000):
         print(f'------- epoch {epoch:>4} -------')
         # training
-        trloader = dataloader.DataLoader(MNIST('./data', True,  transform=trans, download=True), 256, shuffle=False)
+        trloader = dataloader.DataLoader(MNIST('./data', True,  transform=trans, download=True), 8, shuffle=False)
         for i, (image, label) in enumerate(trloader):
             model.zero_grad()
             model.train(image, label)
             optim.step()
-            rolling_acc = rolling_acc * rolling_dam + ((model.predict(image) == label.to(device)) * 1.0).mean().item() * (1-rolling_dam)
+            updater_acc = ((model.predict(image) == label.to(device)) * 1.0).mean().item();
+            rolling_acc = rolling_acc * rolling_dam + updater_acc * (1-rolling_dam)
             print(rolling_acc, end='\r')
         print(f"{rolling_acc:<25} (rolling acc)")
         avg = []
